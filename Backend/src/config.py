a4ffdff7ec53c495997f481d72c3b15abebab00e
AaliyanK <aaliyan.kap@outlook.com>
2021-05-05 21:56:22 -0400

    ''

--- Backend/src/config.py
+++ Backend/src/config.py
@@ -10,7 +10,7 @@ class Development(object):
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-
 app_config = {
     'development': Development,
 }
+
